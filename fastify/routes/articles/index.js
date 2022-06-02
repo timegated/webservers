9bd8f839ec1770a6786ce046d2c192eb29b4afa8
timegated <beccdaniel@gmail.com>
2022-06-01 19:00:30 -0700

    Add readable streams from h-news

---
+++ fastify/routes/articles/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const hnLatestStream = require('hn-latest-stream')
+
+module.exports = async (fastify, opts) => {
+  fastify.get('/', async (request, reply) => {
+    const { amount = 10, type = 'html'} = request.query;
+    
+    // Tell me what the content is or I'll explode
+    // I swear to god.
+    if (type === 'html') reply.type('text/html')
+    if (type === 'json') reply.type('application/json')
+
+    return hnLatestStream(amount, type)
+  })
+};
