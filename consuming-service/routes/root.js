fec2cc1129d1141e9023cf5f51d5803684315d20
timegated <beccdaniel@gmail.com>
2022-06-30 07:36:00 -0700

    Using promise.all to handle multiple requests --slight difference in request time

--- consuming-service/routes/root.js
+++ consuming-service/routes/root.js
@@ -17,9 +17,15 @@ console.log({ brandSrv });
 module.exports = async function (fastify, opts) {
   fastify.get('/:id', async function (request, reply) {
     const { id } = request.params;
-    // Requests in serial
-    const bicycle = await got(`${bicycleSrv}/${id}`).json();
-    const brand = await got(`${brandSrv}/${id}`).json();
+    // Requests in serial -- no noticeable difference between promise all and serial requests
+    // const bicycle = await got(`${bicycleSrv}/${id}`).json();
+    // const brand = await got(`${brandSrv}/${id}`).json();
+    const [bicycle, brand] = await Promise.all([
+      got(`${bicycleSrv}/${id}`).json(),
+      got(`${brandSrv}/${id}`).json(),
+    ]);
+
+    console.log('REQUEST: ', bicycle, brand);
     return {
       id: bicycle.id, // XSS protection, taken from the bicycle object instead of the request
       color: bicycle.color,
