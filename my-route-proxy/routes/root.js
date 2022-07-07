4dc62a821cb7463aa2f82eb56f73b80b63b3c46c
timegated <beccdaniel@gmail.com>
2022-07-06 17:28:04 -0700

    Setting up proxy route

--- my-route-proxy/routes/root.js
+++ my-route-proxy/routes/root.js
@@ -2,6 +2,12 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
+    const { url } = request.query;
+    try {
+      new URL(url); // Main difference when using URL constructor is protocol and origin.
+    } catch (error) {
+      throw fastify.httpErrors.badRequest();
+    }
+    return reply.from(url);
   })
 }
