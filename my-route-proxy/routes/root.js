14f2607f930e8d6f161cee3308db7072ee795ec7
timegated <beccdaniel@gmail.com>
2022-07-06 17:44:30 -0700

    Proxied route set up to capitalize output

--- my-route-proxy/routes/root.js
+++ my-route-proxy/routes/root.js
@@ -1,6 +1,16 @@
 'use strict'
+const {Readable} = require('stream');
 
+async function * upper (res) {
+  for await (const chunk of res) {
+    yield chunk.toString().toUpperCase();
+  }
+}
+
+// Most sites will issue a redirect on proxy
+// This approach better suited for URLS internally
 module.exports = async function (fastify, opts) {
+  // Useful for mapping different endpoints to an upstream service
   fastify.get('/', async function (request, reply) {
     const { url } = request.query;
     try {
@@ -8,6 +18,11 @@ module.exports = async function (fastify, opts) {
     } catch (error) {
       throw fastify.httpErrors.badRequest();
     }
-    return reply.from(url);
+    return reply.from(url, {
+      onResponse (request, reply, res) {
+        console.log('the request: ', request);
+        reply.send(Readable.from(upper(res)));
+      }
+    }); // Promise that resolves once the upstream URL has been sent as a response to the client.
   })
 }
