7a41d97baa7fec555ba918eb04cd8e0ab8866970
timegated <beccdaniel@gmail.com>
2022-07-07 13:36:03 -0700

    Added token to multi-origin proxy

--- my-proxy/app.js
+++ my-proxy/app.js
@@ -3,9 +3,17 @@
 const path = require('path')
 const AutoLoad = require('@fastify/autoload')
 const proxy = require('@fastify/http-proxy');
+const sensible = require('@fastify/sensible')
+
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
+  fastify.register(sensible)
   fastify.register(proxy, {
-    upstream: 'https://news.ycombinator.com'
+    upstream: 'https://news.ycombinator.com',
+    async preHandler(request, reply) {
+      if (request.query.token !== 'abc') {
+        throw fastify.httpErrors.unauthorized()
+      }
+    }
   })
 }
