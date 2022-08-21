d1e3668e83631410c90cbc1df50655c156315010
timegated <beccdaniel@gmail.com>
2022-08-21 07:22:04 -0700

    Added comment explaining parameter pollution

--- server-security/routes/pollution/index.js
+++ server-security/routes/pollution/index.js
@@ -1,5 +1,8 @@
 'use strict'
-
+// Parameters pollution is a vulnerability that appears when we fail to handle
+// query string inputs from a user. Both String and Array can be passed into query params
+// and if we don't handle both cases we could send our services/API into an infite loop.
+// Why it's important to handle errors.
 const someAsynchronousOperation = async (req, res, next) => {
   if (!req.query.name) {
     const err = new Error('Bad Request');
