6448564ce69f644cf2e51e6484fada2466c206a4
timegated <beccdaniel@gmail.com>
2022-08-26 13:00:10 -0700

    Route is working for parsing queryParams

--- myservice/routes/someOther/index.js
+++ myservice/routes/someOther/index.js
@@ -1,36 +1,4 @@
 'use strict'
-// Context dependent solution depending on what needs to be done.
-// I.E. which parts of the name are we using? How do we handle family names? etc.
-const convert = (name) => {
-  const parts = name.split(' ');
-  const last = parts.pop();
-  const first = parts.shift();
-  return { first, last };
-};
-
-
-
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    try {
-      console.log('request: ', request.query);
-      if (!request.query.name) {
-        let err = new Error('Bad Request');
-        err.status = 400;
-        throw new Error(err);
-      }
-      convert(request.query.name);
-      console.log(convert(request.query.name));
-      // probably add a regex here for sanitization purposes
-      return { root: true };
-    } catch (error) {
-      console.error(error);
-    }
-  })
-}
-/**
- * Crud operations
-'use strict'
 const { promisify } = require('util')
 const { bicycle } = require('../../model')
 const { uid } = bicycle
@@ -101,4 +69,3 @@ module.exports = async (fastify, opts) => {
   })
 
 }
- */
