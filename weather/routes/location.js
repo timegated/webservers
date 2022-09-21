69fef143667f15eef596e1aeb0bf95de2af08b89
timegated <beccdaniel@gmail.com>
2022-09-21 08:20:07 -0700

    Working with dynamic request params

--- weather/routes/location.js
+++ weather/routes/location.js
@@ -2,16 +2,22 @@ var express = require('express');
 var request = require('request');
 require('dotenv').config();
 const router = express.Router();
-
+/**
+ * Address format: 2810+bedford,+Los+Angeles,+CA
+ */
 router.get('/', (req, res, next) => {
+  const address = req.params.address;
   const locationData = request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=2810+bedford,+Los+Angeles,+CA&key=${process.env.LOCATION}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.LOCATION}`,
     json: true
   }, (error, response, body) => {
-      console.log(body);
+      console.log(JSON.stringify(body, undefined, 2));
   });
-  res.send('Calling google location api');
-  return locationData;
+  if (address) {
+    res.send('Calling google\'s geolocation api');
+    return locationData;
+  }
+  res.send('We want to call the geolocation api but the address is undefined');
 });
 
 module.exports = router;
