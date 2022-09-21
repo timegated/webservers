6be28b5fd9c5d348f22ac6b06b94bc57d1151bf4
timegated <beccdaniel@gmail.com>
2022-09-21 08:44:03 -0700

    Using query correctly instead of params

--- weather/routes/location.js
+++ weather/routes/location.js
@@ -2,22 +2,25 @@ var express = require('express');
 var request = require('request');
 require('dotenv').config();
 const router = express.Router();
+
 /**
  * Address format: 2810+bedford,+Los+Angeles,+CA
  */
 router.get('/', (req, res, next) => {
-  const address = req.params.address;
-  const locationData = request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.LOCATION}`,
-    json: true
-  }, (error, response, body) => {
-      console.log(JSON.stringify(body, undefined, 2));
-  });
+  const address = req.query.address;
+  console.log(JSON.stringify(req.query));
+  console.log(address);
   if (address) {
-    res.send('Calling google\'s geolocation api');
+    const locationData = request({
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.LOCATION}`,
+      json: true
+    }, (error, response, body) => {
+      console.log(JSON.stringify(body, undefined, 2));
+    });
+    res.send('Calling google location api');
     return locationData;
   }
-  res.send('We want to call the geolocation api but the address is undefined');
+  res.send('Please provide an address');
 });
 
 module.exports = router;
