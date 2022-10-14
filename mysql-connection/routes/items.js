ba23e1ed30226dc358aaa5664ef1f6a40b58667f
timegated <beccdaniel@gmail.com>
2022-10-13 22:35:35 -0700

    Sticking with connection until DAO is fixed

--- mysql-connection/routes/items.js
+++ mysql-connection/routes/items.js
@@ -7,28 +7,31 @@ require('dotenv').config();
 
 const ItemService = require('../services/itemservice');
 
-// var connection = mysql.createConnection({
-//   host: 'winhost',
-//   user: 'root2',
-//   password: process.env.password1,
-//   database: 'wow_test_db',
-//   port: 3306,
-// });
+var connection = mysql.createConnection({
+  host: 'winhost',
+  user: 'root2',
+  password: process.env.password1,
+  database: 'wow_test_db',
+  port: 3306,
+});
+
 const itemService = new ItemService();
+
 /**
  * GET: Fetch items by name.
  */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   if (req.query.name) {
-    
-    // connection.query(`SELECT item_id, name, class, subclass FROM items WHERE name = "${req.query.name}"`, (error, results, fields) => {
-    //   if (error) throw error;
-    //   res.json(results[0]);
-    // });
-   return itemService.getItemByName(req.query.name).then((data) => {
-    console.log('data', data);
-    res.json(data);
-   });
+
+    connection.query(`SELECT item_id, name, class, display_id FROM items WHERE name = "${req.query.name}"`, (error, results, fields) => {
+      if (error) throw error;
+      res.json(results[0]);
+    });
+
+    //  return itemService.getItemByName(req.query.name).then((data) => {
+    //   console.log('data', data);
+    //   res.json(data);
+    //  });
 
   } else {
     throw new Error('name field cannot be empty');
