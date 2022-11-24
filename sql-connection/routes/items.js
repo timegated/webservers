8584096bf126ed1688a8b4b6337a0f4fd4bcdb44
timegated <beccdaniel@gmail.com>
2022-11-23 21:18:32 -0800

    dao working

--- sql-connection/routes/items.js
+++ sql-connection/routes/items.js
@@ -21,7 +21,7 @@ var connection = mysql.createConnection({
 var pgConnection = new pgress.Pool({
   host: 'winhost',
   database: "account_notes",
-  user: "localhost",
+  user: "postgres",
   password: "localhost",
 });
 
@@ -32,14 +32,14 @@ const pgService = new PgService();
  */
 router.get('/', async function (req, res, next) {
   try {
-    // connection.query(`SELECT item_id, name, class, display_id FROM items WHERE name = "${req.query.name}"`, (error, results, fields) => {
-    //   if (error) throw error;
-    //   res.json(results[0]);
-    // });
-     return await itemService.getItemByName(req.query.name).then((data) => {
-      console.log('data', data);
-      res.json(data);
-     });
+    connection.query(`SELECT item_id, name, class, display_id FROM items WHERE name = "${req.query.name}"`, (error, results, fields) => {
+      if (error) throw error;
+      res.json(results[0]);
+    });
+    //  return await itemService.getItemByName(req.query.name).then((data) => {
+    //   console.log('data', data);
+    //   res.json(data);
+    //  });
   } catch (error) {
     throw new Error(error, 'name field cannot be empty');
   }
@@ -48,8 +48,8 @@ router.get('/', async function (req, res, next) {
 
 router.get('/pg', async (req, res) => {
   try {
-    const testQuery = await pgConnection.query('SELECT * from acc_notes');
-    res.json(testQuery);
+    const testQuery = await pgConnection.query('SELECT * from account_notes');
+    res.json(testQuery.rows[0]);
   } catch (error) {
     console.error(error);
   }
